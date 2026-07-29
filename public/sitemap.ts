073462a6870a29a9f4345742c9d684f1993b9858
Tarek Ali <tarekali0@gmail.com>
2026-07-29 11:26:18 +0300

    Add sitemap function to generate URLs

---
+++ public/sitemap.ts
@@ -0,0 +1,12 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://valict.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://valict.com',
+      lastModified: new Date(),
+    },
+  ];
+}
