bf79b48337e50b8410e1e8cd0d4d697a02035581
Tarek Ali <tarekali0@gmail.com>
2026-07-29 12:11:41 +0300

    Add redirect from root to /en

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
