80689e2e215e35257994970adb2a1e873e15e19b
Tarek Ali <tarekali0@gmail.com>
2026-08-13 11:34:04 +0300

    Refactor AboutPage component for improved clarity

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -17,7 +17,7 @@ export async function generateMetadata({
 
   const title =
     currentLang === "ar"
-      ? "من نحن | فالكت"
+      ? "فالكت | من نحن"
       : "Valict | About Us";
 
   const description =
