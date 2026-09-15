18c743619a45976aa6f942dae09e6285012b1a27
Tarek Ali <tarekali0@gmail.com>
2026-09-15 13:02:21 +0300

    Refactor remotePatterns and redirects in config

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,24 @@ const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      { protocol: 'https', hostname: '://unsplash.com', port: '', pathname: '/**' },
+      {
+        protocol: 'https',
+        hostname: '://unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  
-  // الحل السحري لتدمير مشكلة الـ Network dependency tree الخاصة بالأيقونات والمكتبات الكبيرة
-  modularizeImports: {
-    "react-icons/fa6": {
-      transform: "react-icons/fa6/{{member}}",
-      skipDefaultConversion: true,
-    },
-  },
-
   async redirects() {
     return [
-      { source: "/", destination: "/en", permanent: true },
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
     ];
   },
 };
